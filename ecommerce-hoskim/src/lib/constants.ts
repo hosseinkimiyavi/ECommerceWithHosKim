ed4cb7c105b15b8249f021763fdbc331351ded76
Hossein Kimiyavi <hosseinkimiyavi@gmail.com>
2025-11-04 11:21:37 +0330

    install react medium image zoom and install separator from shadcn and add pageNumber constant and create getproductbySlug and Category function in product action file and create productGallery and selectVariant Component

--- ecommerce-hoskim/src/lib/constants.ts
+++ ecommerce-hoskim/src/lib/constants.ts
@@ -1,3 +1,5 @@
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "E-Commerce-HosKim";
 export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || "E-Commerce Project by HosKim.";
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || "This is an E-Commerce project built with Next.js and Tailwind CSS and MongoDB by HosKim.";
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
